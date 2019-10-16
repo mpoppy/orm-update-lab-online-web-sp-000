a00984cfd21a646e07d4a78eb1faa7c97dcd22a5
Marc Poppy <poppy.marc@gmail.com>
2019-10-16 03:33:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,9 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :name, :age
+  attr_reader :id 
+
   def initialize (id=nil, name, age)
     @id = id
     @name = name
