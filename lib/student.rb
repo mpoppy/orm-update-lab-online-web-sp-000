bfd326d74a4d8097c07e5bbf0d437fb4d219132f
Marc Poppy <poppy.marc@gmail.com>
2019-10-16 03:44:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,9 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.new_from_db
 
   end
 
