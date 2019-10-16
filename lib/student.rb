a2598a1959129de2f968d48d3a190127134039dd
Marc Poppy <poppy.marc@gmail.com>
2019-10-16 03:32:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,11 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize (id=nil, name, age)
+    @id = id
+    @name = name
+    @age = age
+  end
 
 
 end
