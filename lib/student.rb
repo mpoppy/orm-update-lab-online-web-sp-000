aeaad248adb70d54c5c0a53d5eb6d742f21747d9
Marc Poppy <poppy.marc@gmail.com>
2019-10-16 03:34:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,13 @@ class Student
   end
 
   def self.create_table
+    sql = <<-SQL
+    CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
 
   end
 
